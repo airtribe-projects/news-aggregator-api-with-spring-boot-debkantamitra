b83f20da3ec1788b5adc3d08efb542e5e4f6abfb
Debkanta Mitra <debkanta.mitra@leapfinance.com>
2024-11-11 00:46:38 +0530

    setup

---
+++ src/main/java/com/debkanta/projects/NewAggretorAPI/NewAggretorApiApplication.java
@@ -0,0 +1,11 @@
+package com.debkanta.projects.NewAggretorAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAggretorApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NewAggretorApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/debkanta/projects/NewAggretorAPI/NewAggretorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debkanta.projects.NewAggretorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAggretorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
